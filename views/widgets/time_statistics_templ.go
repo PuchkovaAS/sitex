74057d52692fffd0f97396df2d4830773d0b1f3a
PuchkovaAS <puchkovaac@rambler.ru>
2025-08-25 15:28:35 +0300

    little fix

--- views/widgets/time_statistics_templ.go
+++ views/widgets/time_statistics_templ.go
@@ -96,9 +96,9 @@ func TimeStatistics(stats map[string]int) templ.Component {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var6 string
-		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(stats["Коммандировка"])
+		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(stats["Командировка"])
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/widgets/time_statistics.templ`, Line: 48, Col: 88}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/widgets/time_statistics.templ`, Line: 48, Col: 86}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 		if templ_7745c5c3_Err != nil {
